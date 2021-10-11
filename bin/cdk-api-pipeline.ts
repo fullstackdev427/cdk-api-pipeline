251d6ae64fc0b78ebbe37416bd0cee0735293403
fullstackdev427 <bear_star713@hotmail.com>
2021-10-11 12:58:46 +0900

    initial commit

---
+++ bin/cdk-api-pipeline.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { LambdaStack } from '../lib/lambda-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+if (!process.env.GITHUB_TOKEN) {
+  console.log("No Github Token present");
+}
+
+const app = new cdk.App();
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+  env: {
+    region: "ap-northeast-1"
+  }
+});
+new PipelineStack(app, 'PipelineStack', {
+  lambdaCode: lambdaStack.lambdaCode,
+  githubToken: process.env.GITHUB_TOKEN || "",
+  env: {
+    region: "ap-northeast-1",
+  }
+});
+
+app.synth();
